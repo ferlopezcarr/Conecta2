61122fafeb11a9d27cdf7c901f17adc5438f7b06
DESKTOP-UBIK15G\Jorge <jorgemer@ucm.es>
2018-03-11 21:51:42 +0100

    data sql renombrado

--- src/main/resources/data-mysql.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `roles`(`nombre`) VALUES ('Usuario');
-INSERT INTO `roles`(`nombre`) VALUES ('Empresa');
-INSERT INTO `roles`(`nombre`) VALUES ('Administrador');
-
-INSERT INTO `usuarios`(`nombre`, `apellidos`, `email`, `activo`, `password`) VALUES ('Administrador', '', 'admin@admin.es', 1, '$2a$10$qXgak7rkAqGqkJjzxT6/Ru0hHGgoGyN81Cobog4yZmO4v.y6cNrbO');
-INSERT INTO `usuario_rol`(`id_rol`, `id_usuario`) VALUES (3, 1);
