60a1ce39493dc5e7d2693fa71de843831e8b1658
ferlopezcarr <ferlopezcarr@gmail.com>
2018-03-23 12:35:29 +0100

    efef

---
+++ src/test/java/testPrueba/TestPruebaTest.java
@@ -0,0 +1,15 @@
+package testPrueba;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPruebaTest {
+
+	@Test
+	public void test() {
+		//test que falla
+		fail("Not yet implemented");
+	}
+
+}
