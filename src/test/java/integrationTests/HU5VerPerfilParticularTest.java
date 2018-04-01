a979f333aad6eb6c24e44221e5a1d5c647488a1a
ferlopezcarr <ferlopezcarr@gmail.com>
2018-04-02 01:53:15 +0200

    Test unidad e integración con JUnit HU5VerPerfilParticular

---
+++ src/test/java/integrationTests/HU5VerPerfilParticularTest.java
@@ -0,0 +1,64 @@
+package integrationTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import conecta2.Application;
+import conecta2.modelo.Particular;
+import conecta2.servicioAplicacion.SAParticular;
+import conecta2.transfer.TransferParticular;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@DataJpaTest
+@ComponentScan(basePackages ="conecta2")
+public class HU5VerPerfilParticularTest {
+
+	@Autowired
+	public SAParticular saParticular;
+	
+	
+	@Test
+	public void testEmpresaFoundedByEmailYActivo() {
+		
+		TransferParticular particular = new TransferParticular("nombre", "Apellido Apellido", "99999999Z", "particularPruebaEmail@particularPruebaEmail.com", "Abc1111", true, 0);
+		
+		saParticular.save(particular);
+
+		Particular particularBD = saParticular.buscarPorEmail(particular.getEmail());
+		
+		boolean email = particularBD.getEmail() == particular.getEmail();
+		boolean nombre = particularBD.getNombre() == particular.getNombre();
+		boolean apellidos = particularBD.getApellidos() == particular.getApellidos();
+		boolean dni = particularBD.getDni() == particular.getDni();
+		boolean activo = (particularBD.getActivo() == particular.getActivo()) == true;
+		
+		assertEquals(true, (email && nombre && apellidos && dni && activo));
+	}
+	
+	@Test
+	public void testEmpresaFoundedByCifYActivo() {
+		
+		TransferParticular particular = new TransferParticular("nombre", "Apellido Apellido", "99999999Z", "particularPruebaEmail@particularPruebaEmail.com", "Abc1111", true, 0);
+		
+		saParticular.save(particular);
+
+		Particular particularBD = saParticular.buscarPorDni(particular.getDni());
+		
+		boolean email = particularBD.getEmail() == particular.getEmail();
+		boolean nombre = particularBD.getNombre() == particular.getNombre();
+		boolean apellidos = particularBD.getApellidos() == particular.getApellidos();
+		boolean dni = particularBD.getDni() == particular.getDni();
+		boolean activo = (particularBD.getActivo() == particular.getActivo()) == true;
+		
+		assertEquals(true, (email && nombre && apellidos && dni && activo));
+	}
+
+}
---
+++ src/test/java/unitTests/HU5VerPerfilParticularTest.java
@@ -0,0 +1,60 @@
+package unitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import conecta2.Application;
+import conecta2.dao.DAOParticular;
+import conecta2.modelo.Particular;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@DataJpaTest
+public class HU5VerPerfilParticularTest {
+
+	@Autowired
+	private DAOParticular daoParticular;
+	
+	@Test
+	public void testEmpresaFoundedByEmailYActivo() {
+		
+		Particular particular = new Particular("nombre", "Apellido Apellido", "99999999Z", "particularPruebaEmail@particularPruebaEmail.com", "Abc1111", true, 0);
+
+		daoParticular.save(particular);
+		
+		Particular particularBD = daoParticular.findByEmail(particular.getEmail());
+		
+		boolean email = particularBD.getEmail() == particular.getEmail();
+		boolean nombre = particularBD.getNombre() == particular.getNombre();
+		boolean apellidos = particularBD.getApellidos() == particular.getApellidos();
+		boolean dni = particularBD.getDni() == particular.getDni();
+		boolean activo = (particularBD.getActivo() == particular.getActivo()) == true;
+		
+		assertEquals(true, (email && nombre && apellidos && dni && activo));
+	}
+	
+	@Test
+	public void testEmpresaFoundedByCifYActivo() {
+		
+		Particular particular = new Particular("nombre", "Apellido Apellido", "99999999Z", "particularPruebaEmail@particularPruebaEmail.com", "Abc1111", true, 0);
+		
+		daoParticular.save(particular);
+		
+		Particular particularBD = daoParticular.findByDni(particular.getDni());
+		
+		boolean email = particularBD.getEmail() == particular.getEmail();
+		boolean nombre = particularBD.getNombre() == particular.getNombre();
+		boolean apellidos = particularBD.getApellidos() == particular.getApellidos();
+		boolean dni = particularBD.getDni() == particular.getDni();
+		boolean activo = (particularBD.getActivo() == particular.getActivo()) == true;
+		
+		assertEquals(true, (email && nombre && apellidos && dni && activo));
+	}
+
+}
