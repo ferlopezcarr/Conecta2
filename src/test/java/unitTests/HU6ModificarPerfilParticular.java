d82d2a81f3e34d6b6c4630aff653d1ffe186a254
Pabloo17 <pablochester92@gmail.com>
2018-04-05 23:26:26 +0200

    pruebas unitarias modificarperfilparticular

---
+++ src/test/java/unitTests/HU6ModificarPerfilParticular.java
@@ -0,0 +1,39 @@
+package unitTests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import conecta2.Application;
+import conecta2.dao.DAOParticular;
+import conecta2.modelo.Particular;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@DataJpaTest
+public class HU6ModificarPerfilParticular {
+	
+	@Autowired
+    private DAOParticular daoParticular;
+	
+	@Test
+	public void testModifyExistingParticular() {
+		
+		Particular particular = new Particular("nombre", "Apellido Apellido", "99999999Z", "123456789", "particularPruebaEmail@particularPruebaEmail.com", "Abc1111", true, 0);
+		
+		daoParticular.save(particular);
+		
+		particular.setNombre("nombreModificado");
+		particular.setEmail("otroEmail@particular.com");
+		
+		Particular particularGuardado = daoParticular.save(particular);
+		
+		assertEquals(particular, particularGuardado);
+	}
+
+}
