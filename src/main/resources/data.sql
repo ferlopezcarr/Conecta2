454a350cc781aae1ed02d7179be96b637cf80ac0
ferlopezcarr <ferlopezcarr@gmail.com>
2018-04-07 18:17:41 +0200

    arreglados tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,2 @@
 INSERT INTO `particulares`(`dni`, `nombre`, `apellidos`, `telefono`, `activo`, `puntuacion`, `email`, `password`) VALUES ('12345678L', 'Luis', 'Perez', 123456789, 1, 0, 'luis@gmail.es', '$2a$10$qXgak7rkAqGqkJjzxT6/Ru0hHGgoGyN81Cobog4yZmO4v.y6cNrbO');
 INSERT INTO `empresas` (`cif`, `nombre_empresa`, `telefono`, `activo`, `puntuacion`, `email`, `password`) VALUES ('A97329148', 'Endesa', 123456789, 1, 0, 'endesa@gmail.es', '$2a$10$qXgak7rkAqGqkJjzxT6/Ru0hHGgoGyN81Cobog4yZmO4v.y6cNrbO');
-INSERT INTO `ofertas` (`activo`, `ciudad`, `contrato`, `descripcion`, `jornada`, `nombre`, `salario`, `vacantes`, `empresa_id`) VALUES
-(b'0', 'Talavera de la Reina', 0, 'Programador Java, con al menos 2 años de experiencia, que no use margin 0 auto en los html', 0, 'Programador Java', 1500, 24, 1);
-INSERT INTO `ofertas` (`activo`, `ciudad`, `contrato`, `descripcion`, `jornada`, `nombre`, `salario`, `vacantes`, `empresa_id`) VALUES
-( b'0', 'Talavera de la Reina', 0, 'Programador Java, con al menos 2 años de experiencia, que no use margin 0 auto en los html', 0, 'Java ', 1500, 24, 1);
-INSERT INTO `ofertas` ( `activo`, `ciudad`, `contrato`, `descripcion`, `jornada`, `nombre`, `salario`, `vacantes`, `empresa_id`) VALUES
-( b'0', 'Talavera de la Reina', 0, 'Programador Java, con al menos 2 años de experiencia, que no use margin 0 auto en los html', 0, 'Java ', 1500, 24, 1);
